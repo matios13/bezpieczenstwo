45fb9413b184f6b327b3c6d99246fc9ddad6ac9e
Zbigniew Czarnecki <209909@student.pwr.edu.pl>
2016-10-10 19:03:16 +0200

    Add Gson to Server

--- lab1/src/Server/WorkerRunnable.java
+++ lab1/src/Server/WorkerRunnable.java
@@ -1,10 +1,13 @@
 package Server;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
+import com.google.gson.Gson;
 /**
 
  */
@@ -12,6 +15,11 @@ public class WorkerRunnable implements Runnable{
 
     protected Socket clientSocket = null;
     protected String serverText   = null;
+    private Integer p = 23;
+    private Integer g = 5;
+    private Integer A;
+    private Integer B;
+    boolean step1 = false;
 
     public WorkerRunnable(Socket clientSocket, String serverText) {
         this.clientSocket = clientSocket;
@@ -22,16 +30,24 @@ public class WorkerRunnable implements Runnable{
         try {
             InputStream input  = clientSocket.getInputStream();
             OutputStream output = clientSocket.getOutputStream();
-            long time = System.currentTimeMillis();
+            
+            //STEP 1
+            while(!step1){
+            	BufferedReader streamReader = new BufferedReader(new InputStreamReader(input, "UTF-8")); 
+            	StringBuilder responseStrBuilder = new StringBuilder();
+
+            	String inputStr;
+            	while ((inputStr = streamReader.readLine()) != null)
+            	    responseStrBuilder.append(inputStr);
+            	//Gson pAndG =
+            	//new Gson(responseStrBuilder.toString());
+            }
             output.write(("HTTP/1.1 200 OK\n\nWorkerRunnable: " +
-this.serverText + " - " +
-time +
-"").getBytes());
+this.serverText + " - " +"").getBytes());
             output.close();
             input.close();
-            System.out.println("Request processed: " + time);
+            System.out.println("Request processed: ");
         } catch (IOException e) {
-            //report exception somewhere.
             e.printStackTrace();
         }
     }
