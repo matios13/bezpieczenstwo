9e727e085ece45aaff2b4b6daf00e3cd7fd58338
matios13 <matios13@gmail.com>
2016-11-22 21:47:03 +0100

    Funkcja atoi + Random jak nei liczba

--- lab4/Keygen/src/Main.java
+++ lab4/Keygen/src/Main.java
@@ -1,8 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Created by matio_000 on 22.11.2016.
  */
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println("Lamacz hasel 1.0");
+        System.out.println("P.S nie jest to metoda bruta forsa :P ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj nr indeksu:\n");
+        String indeks = scanner.nextLine();
+        System.out.println(atoi(indeks));
+    }
+    private static int atoi(String co){
+        try{
+            return new Integer(co);
+        }catch (NumberFormatException e){
+            System.err.println("Nie podales liczby!!!!!!\n" +
+                    "Będzie RANDOM!");
+            return new Random().nextInt();
+        }
+
     }
 }
